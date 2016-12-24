806558a4aaac656600ab36ab1b0958dd2f46fc5e
martinnett <martin_nett@icloud.com>
2016-12-24 17:17:23 +0800

    Update application whole layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def full_title(page)
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page
+      page + ' | ' + base_title
+    else
+      page
+    end
+  end
 end
