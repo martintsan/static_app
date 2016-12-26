d8c9b6459a9f3d6ac21e4e6385120ff6c51a0b55
martinnett <martin_nett@icloud.com>
2016-12-26 17:42:37 +0800

    Add user model & validations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+            length: { maximum: 50 }
+  validates :email, presence: true,
+            length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i },
+            uniqueness: { case_sensitive: false }
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Martin Nett", email: "martin_nett@icloud.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = 'a' * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email should accept valid addresses" do
+    valid_addresses = %w[martin@example.com
+      MARIN_NETT@icloud.com foo-bar@foo.bar.org
+      foo.bar@foo.bar.org foo+bar@foo.bar.org]
+    valid_addresses.each do |email|
+      @user.email = email
+      assert @user.valid?, "#{email.inspect} should be valid"
+    end
+  end
+
+  test "email should reject invalid addresses" do
+    invalid_addresses = %w[martin@example,com
+      MARIN_NETT_at_icloud.com foo-bar@foo.bar@org.cn
+      foobar@foo+bar.org]
+    invalid_addresses.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.downcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+end
