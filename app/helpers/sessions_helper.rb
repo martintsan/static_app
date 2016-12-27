9e2b866dd41183b717b1006847cbd48e3e56e3a6
martinnett <martin_nett@icloud.com>
2016-12-27 17:31:01 +0800

    Update views for logged users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,12 @@ module SessionsHelper
   def login(user)
     session[:user_id] = user.id
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
